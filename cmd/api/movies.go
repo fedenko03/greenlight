47052e046923c32ffdeb9afe35052172e4e733ac
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-12-13 13:26:05 +0600

    Validating JSON Input

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"greenlight.aitu.kz/internal/data"
+	"greenlight.aitu.kz/internal/validator"
 	"net/http"
 	"time"
 )
@@ -17,6 +18,17 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	v := validator.New()
+	if data.ValidateMovie(v, movie); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 	fmt.Fprintf(w, "%+v\n", input)
