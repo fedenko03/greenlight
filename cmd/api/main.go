f2c98c69a6ddc59aff856ed207de2a2cf356c88a
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-12-20 16:05:33 +0600

    Database Setup and Configuration (pgx)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/jackc/pgx/v5/stdlib"
 	"log"
 	"net/http"
 	"os"
@@ -14,6 +17,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -25,8 +34,22 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
+
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	pool, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		log.Fatalf("Unable to connection to database: %v\n", err)
+	}
+	defer pool.Close()
+
+	log.Println("Connected!")
+
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -41,6 +64,6 @@ func main() {
 	}
 
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
