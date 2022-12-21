5ce5ea75890c9f111e2d5fb0992898fd9f1f6934
alexey F <52556121+fedenko03@users.noreply.github.com>
2022-12-21 18:14:00 +0600

    SQL Migrations

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,7 +12,7 @@ import (
 	"time"
 )
 
-const version = "1.0.0"
+const version = "1.0.1"
 
 type config struct {
 	port int
@@ -31,6 +31,7 @@ type application struct {
 }
 
 func main() {
+
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
@@ -41,29 +42,43 @@ func main() {
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 
 	flag.Parse()
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-	pool, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	pool, err := openDB(cfg)
 	if err != nil {
-		log.Fatalf("Unable to connection to database: %v\n", err)
+		logger.Fatal(err)
 	}
 	defer pool.Close()
-
-	log.Println("Connected!")
-
+	logger.Printf("database connection pool established")
 	app := &application{
 		config: cfg,
 		logger: logger,
 	}
-
 	srv := &http.Server{
-		Addr:         fmt.Sprintf("localhost:%d", cfg.port),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
-
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+
+	pool, err := pgxpool.New(context.Background(), cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	pool.Config().MaxConnIdleTime, err = time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	pool.Config().MaxConns = int32(cfg.db.maxOpenConns)
+
+	return pool, nil
 }
