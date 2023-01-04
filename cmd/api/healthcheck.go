76f86c050c3677089568f195d743b82296672b37
alexey F <52556121+fedenko03@users.noreply.github.com>
2023-01-04 20:02:02 +0600

    Some changes

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,15 +5,17 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	env := envelope{
+	data := envelope{
 		"status": "available",
 		"system_info": map[string]string{
 			"environment": app.config.env,
 			"version":     version,
 		},
 	}
-	err := app.writeJSON(w, http.StatusOK, env, nil)
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.serverErrorResponse(w, r, err)
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
 	}
 }
